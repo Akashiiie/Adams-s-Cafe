f15b8bb2b0012252f8eca0c417f98895bebd6149
Akashiiie <gepa.castres.up@phinmaed.com>
2025-03-21 19:34:08 +0800

    Add files via upload

---
+++ config/coffeconnection.php
@@ -0,0 +1,31 @@
+<?php
+class CoffeDatabase{
+    private static $instance = null;
+    private $conn;
+    private $host = 'localhost';
+    private $db_name = 'userdb';
+    private $db_user= 'root';
+    private $db_pass = '';
+
+    // Singleton Pattern 
+    private function __construct() {
+
+        
+        // Correcting the order of parameters
+        $this->conn = new mysqli($this->host, $this->db_user, $this->db_pass, $this->db_name);
+        
+        // Check for connection errors
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    // Get single instance of Database connection
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new CoffeDatabase();
+        }
+        return self::$instance->conn;
+    }
+}
+?>
