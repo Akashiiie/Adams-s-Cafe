33e9ce01c4e80e1855b08f5f28535deef9ff49e8
Akashiiie <gepa.castres.up@phinmaed.com>
2025-03-25 10:08:01 +0800

    Delete api directory

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/json");
-
-//controllers 
-require_once '../controller/admin.php';
-require_once '../controller/admincontroller.php';
-require_once '../controller/records.php';
-require_once '../controller/users.php';
-
